4c71a4907218c2fb2fc4702cef7de5b80ca3f19a
MRREEY <schraagenreymark@gmail.com>
2022-06-14 13:53:10 +0200

    Deleted content React

--- my-app/src/components/Button.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-class Button extends Component {
-  render() {
-    return <Button color="red" />;
-  }
-}
-
-export default Button; // Don’t forget to use export default!
