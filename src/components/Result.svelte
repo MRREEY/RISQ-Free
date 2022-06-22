0d8168e73c0849637b5312c7cf4fe705b17b08ba
Qwinsie <theqpowerrr@gmail.com>
2022-06-22 21:11:34 +0200

    Results screen with css.

--- src/components/Result.svelte
+++ src/components/Result.svelte
@@ -1,12 +1,36 @@
 <script>
     export let answers;
+    export let index;
 </script>
 
-<h1>Bedankt voor jullie antwoorden!</h1>
-<h2>De menigte vind deze stelling:</h2>
-<ul>
-    {#each Object.entries(answers) as [answer, percentage] }
-        <li>{percentage} {answer}</li>
-    {/each}
-</ul>
-<button on:click>Volgende</button>
+<div class="resultScreen">
+    <section class="background"></section>
+    <h1 class="title">Bedankt voor jullie antwoorden!</h1>
+    <div class="content">
+        <h2>De menigte vind deze stelling:</h2>
+        <ul>
+            {#each Object.entries(answers) as [answer, percentage] }
+                <li>{percentage} {answer}</li>
+            {/each}
+        </ul>
+        <button on:click>Volgende</button>
+    </div>
+    <p class="pagination">{index + 1}/3</p>
+</div>
+
+<style>
+
+.resultScreen {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    align-content: space-around;
+    text-align: center;
+    height:100vh
+}
+
+h2 {
+    font-family: "Montserrat Bold";
+}
+</style>
