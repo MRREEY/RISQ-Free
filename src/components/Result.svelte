8b1e44f6288fe0e260fe4f54bfb08a72cba3bcb2
Qwinsie <theqpowerrr@gmail.com>
2022-06-22 16:44:29 +0200

    Result component.

---
+++ src/components/Result.svelte
@@ -0,0 +1,12 @@
+<script>
+    export let answers;
+</script>
+
+<h1>Bedankt voor jullie antwoorden!</h1>
+<h2>De menigte vind deze stelling:</h2>
+<ul>
+    {#each Object.entries(answers) as [answer, percentage] }
+        <li>{percentage} {answer}</li>
+    {/each}
+</ul>
+<button on:click>Volgende</button>
