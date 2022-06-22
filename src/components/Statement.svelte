2e2772120c9b25a0c334322c14f86c4899bceb9e
Qwinsie <theqpowerrr@gmail.com>
2022-06-22 21:11:54 +0200

    adding different alternative screens.

--- src/components/Statement.svelte
+++ src/components/Statement.svelte
@@ -3,40 +3,64 @@
     export let title;
     export let answers;
     export let alternativeScreen
+    export let alternativeScreenType
     
+    const alternativeScreenTypes = ["slider", "textfield", "nothing"]
+
     let showAlternativeScreen = false
 </script>
 
 <div class="statementScreen">
-    <h1>{title}</h1>
-    <img alt="cartoon storyboard" class="storyboard" src="/images/storyboards/{index}.png"/>
-    {#if !showAlternativeScreen}
-        {#each Object.entries(answers) as [answer], index }
-            <button
-                class="button"
-                on:click
-                class:green={index === 0}
-                class:red={index === 1}
-                >{answer}
-            </button>
-        {/each}
-        <span on:click={ () => {showAlternativeScreen = !showAlternativeScreen}}>{alternativeScreen}</span>
-    {:else}
-        <input type="range" list="tickmarks" step="20">
-        <datalist>
-            {#each [0, 1, 2, 3, 4, 5] as value}
-                {#if value == 0}
-                    <option label="intimiderend" value="{value*1}"></option> 
-                {:else if value == 5}
-                    <option label="complimenterend" value="{value*1}"></option>
-                {:else}
-                    <option value="{value*1}"></option>
-                {/if}
+    <section class="background"></section>
+    <h1 class="title">{title}</h1>
+    <div class="content">
+        <img alt="cartoon storyboard" class="storyboard" src="/images/storyboards/{index}.png"/>
+        {#if !showAlternativeScreen}
+            {#each Object.entries(answers) as [answer], index }
+                <button
+                    class="button"
+                    on:click
+                    class:green={index === 0}
+                    class:red={index === 1}
+                    >{answer}
+                </button>
             {/each}
-        </datalist>
-        <button on:click >opslaan</button>
-        <button on:click={ () => {showAlternativeScreen = !showAlternativeScreen}}>Terug</button>
-    {/if}
+            {#if alternativeScreenType == "nothing" }
+                <span on:click>{alternativeScreen}</span>
+            {:else}
+                <span on:click={ () => {showAlternativeScreen = !showAlternativeScreen}}>{alternativeScreen}</span>
+            {/if}
+        {:else if alternativeScreenType == "slider"}
+            <div class="emojis">
+                <img class="emoji" src="/images/logos/face-frown.png" alt="sad emoji">
+                <img class="emoji" src="/images/logos/face-smile.png" alt="happy emoji">
+            </div>
+            <datalist id="steplist">
+                {#each [0, 1, 2, 3, 4, 5] as value}
+                    {#if value == 0}
+                        <option label="intimiderend" value="{value*10}"></option> 
+                    {:else if value == 5}
+                        <option label="complimenterend" value="{value*10}"></option>
+                    {:else}
+                        <option value="{value*10}"></option>
+                    {/if}
+                {/each}
+            </datalist>
+            <input type="range" class="slider" min="0" max="100" list="steplist" step="20">
+            <datalist id="steplist">
+                {#each [0, 1, 2, 3, 4, 5] as value}
+                    <option value="{value*10}">|</option>
+                {/each}
+            </datalist>
+            <button on:click >opslaan</button>
+            <button on:click={ () => {showAlternativeScreen = !showAlternativeScreen}}>Terug</button>
+        {:else if alternativeScreenType == "textfield"}
+            <textarea name="feedback" id="feedback" cols="30" placeholder="Vul in..." rows="10"></textarea>
+            <button on:click >opslaan</button>
+            <button on:click={ () => {showAlternativeScreen = !showAlternativeScreen}}>Terug</button>
+        {/if}
+    </div>
+    <p class="pagination">{index + 1}/3</p>
 </div>
 
 <style>
@@ -51,17 +75,80 @@
     height:100vh
 }
 
+.emojis {
+    display: flex;
+    justify-content: space-between;
+}
+.emoji {
+    width: 30px;
+
+}
+
+
+datalist {
+    display: flex;
+    justify-content: space-between;
+}
+
+option {
+    color: grey;
+}
+
+input {
+    width: 50%;
+}
+
+.slider {
+  -webkit-appearance: none;
+  width: 100%;
+  height: 15px;
+  border-radius: 5px;
+  background: #d3d3d3;
+  outline: none;
+  opacity: 0.7;
+  -webkit-transition: .2s;
+  transition: opacity .2s;
+}
+
+.slider:hover {
+  opacity: 1;
+}
+
+.slider::-webkit-slider-thumb {
+  -webkit-appearance: none;
+  appearance: none;
+  width: 25px;
+  height: 25px;
+  border-radius: 50%;
+  background: #00811f;
+  cursor: pointer;
+}
+
+.slider::-moz-range-thumb {
+  width: 25px;
+  height: 25px;
+  border-radius: 50%;
+  background: #00811f;
+  cursor: pointer;
+}
+
+img {
+    width: 100%;
+    margin-bottom: 30px;
+}
+
 span {
     text-decoration: underline;
 }
 
-
 .green {
+    text-transform: Uppercase;
     color: #00811f;
     border-color: #00811f;
 }
 
 .red {
+    text-transform: Uppercase;
     color: #FF5C50;
     border-color: #FF5C50;
 }
