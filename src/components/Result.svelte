51f985b5b44c276411be9193425b59fc53a56404
MRREEY <schraagenreymark@gmail.com>
2022-06-23 11:47:34 +0200

    Buttons styled

--- src/components/Result.svelte
+++ src/components/Result.svelte
@@ -1,36 +1,44 @@
 <script>
-    export let answers;
-    export let index;
+	export let answers;
+	export let index;
 </script>
 
 <div class="resultScreen">
-    <section class="background"></section>
-    <h1 class="title">Bedankt voor jullie antwoorden!</h1>
-    <div class="content">
-        <h2>De menigte vind deze stelling:</h2>
-        <ul>
-            {#each Object.entries(answers) as [answer, percentage] }
-                <li>{percentage} {answer}</li>
-            {/each}
-        </ul>
-        <button on:click>Volgende</button>
-    </div>
-    <p class="pagination">{index + 1}/3</p>
+	<section class="background" />
+	<h1 class="title">Bedankt voor jullie antwoorden!</h1>
+	<div class="content">
+		<h2>De menigte vind deze stelling:</h2>
+		<ul>
+			{#each Object.entries(answers) as [answer, percentage]}
+				<li>{percentage} {answer}</li>
+			{/each}
+		</ul>
+		<button on:click class="volgendeButton">Volgende</button>
+	</div>
+	<p class="pagination">{index + 1}/3</p>
 </div>
 
 <style>
+	.resultScreen {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		align-content: space-around;
+		text-align: center;
+		height: 100vh;
+	}
 
-.resultScreen {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    align-content: space-around;
-    text-align: center;
-    height:100vh
-}
+	h2 {
+		font-family: 'Montserrat Bold';
+	}
 
-h2 {
-    font-family: "Montserrat Bold";
-}
+	.volgendeButton {
+		background-color: #00811f;
+		color: white;
+		border-radius: 4px;
+		font-size: 27px;
+		-ms-transform: translate(-50%, 50%);
+		transform: translate(5%, 50%);
+	}
 </style>
--- src/components/Statement.svelte
+++ src/components/Statement.svelte
@@ -1,182 +1,172 @@
 <script>
-    export let index;
-    export let title;
-    export let answers;
-    export let alternativeScreen
-    export let alternativeScreenType
-    
-    const alternativeScreenTypes = ["slider", "textfield", "nothing"]
+	export let index;
+	export let title;
+	export let answers;
+	export let alternativeScreen;
+	export let alternativeScreenType;
 
-    let showAlternativeScreen = false
+	const alternativeScreenTypes = ['slider', 'textfield', 'nothing'];
+
+	let showAlternativeScreen = false;
 </script>
 
 <div class="statementScreen">
-    <section class="background"></section>
-    <h1 class="title">{title}</h1>
-    <div class="content">
-        <img alt="cartoon storyboard" class="storyboard" src="/images/storyboards/{index}.png"/>
-        {#if !showAlternativeScreen}
-            {#each Object.entries(answers) as [answer], index }
-                <button
-                    class="button"
-                    on:click
-                    class:green={index === 0}
-                    class:red={index === 1}
-                    >{answer}
-                </button>
-            {/each}
-            {#if alternativeScreenType == "nothing" }
-                <span on:click>{alternativeScreen}</span>
-            {:else}
-                <span on:click={ () => {showAlternativeScreen = !showAlternativeScreen}}>{alternativeScreen}</span>
-            {/if}
-        {:else if alternativeScreenType == "slider"}
-            <div class="emojis">
-                <img class="emoji" src="/images/logos/face-frown.png" alt="sad emoji">
-                <img class="emoji" src="/images/logos/face-smile.png" alt="happy emoji">
-            </div>
-            <datalist id="steplist">
-                {#each [0, 1, 2, 3, 4, 5] as value}
-                    {#if value == 0}
-                        <option label="intimiderend" value="{value*10}"></option> 
-                    {:else if value == 5}
-                        <option label="complimenterend" value="{value*10}"></option>
-                    {:else}
-                        <option value="{value*10}"></option>
-                    {/if}
-                {/each}
-            </datalist>
-            <input type="range" class="slider" min="0" max="100" list="steplist" step="20">
-            <datalist id="steplist">
-                {#each [0, 1, 2, 3, 4, 5] as value}
-                    <option value="{value*10}">|</option>
-                {/each}
-            </datalist>
-            <button on:click >opslaan</button>
-            <button on:click={ () => {showAlternativeScreen = !showAlternativeScreen}}>Terug</button>
-        {:else if alternativeScreenType == "textfield"}
-            <textarea name="feedback" id="feedback" cols="30" placeholder="Vul in..." rows="10"></textarea>
-            <button class="opslaanButton" on:click >opslaan</button>
-            <button class="terugButton" on:click={ () => {showAlternativeScreen = !showAlternativeScreen}}>Terug</button>
-        {/if}
-    </div>
-    <p class="pagination">{index + 1}/3</p>
+	<section class="background" />
+	<h1 class="title">{title}</h1>
+	<div class="content">
+		<img alt="cartoon storyboard" class="storyboard" src="/images/storyboards/{index}.png" />
+		{#if !showAlternativeScreen}
+			{#each Object.entries(answers) as [answer], index}
+				<button class="button" on:click class:green={index === 0} class:red={index === 1}
+					>{answer}
+				</button>
+			{/each}
+			{#if alternativeScreenType == 'nothing'}
+				<span on:click>{alternativeScreen}</span>
+			{:else}
+				<span
+					on:click={() => {
+						showAlternativeScreen = !showAlternativeScreen;
+					}}>{alternativeScreen}</span
+				>
+			{/if}
+		{:else if alternativeScreenType == 'slider'}
+			<div class="emojis">
+				<img class="emoji" src="/images/logos/face-frown.png" alt="sad emoji" />
+				<img class="emoji" src="/images/logos/face-smile.png" alt="happy emoji" />
+			</div>
+			<datalist id="steplist">
+				{#each [0, 1, 2, 3, 4, 5] as value}
+					{#if value == 0}
+						<option label="intimiderend" value={value * 10} />
+					{:else if value == 5}
+						<option label="complimenterend" value={value * 10} />
+					{:else}
+						<option value={value * 10} />
+					{/if}
+				{/each}
+			</datalist>
+			<input type="range" class="slider" min="0" max="100" list="steplist" step="20" />
+			<datalist id="steplist">
+				{#each [0, 1, 2, 3, 4, 5] as value}
+					<option value={value * 10}>|</option>
+				{/each}
+			</datalist>
+			<button class="buttonsOT" on:click>opslaan</button>
+			<button
+				class="buttonsOT"
+				on:click={() => {
+					showAlternativeScreen = !showAlternativeScreen;
+				}}>Terug</button
+			>
+		{:else if alternativeScreenType == 'textfield'}
+			<textarea name="feedback" id="feedback" cols="30" placeholder="Vul in..." rows="10" />
+			<button on:click class="opslaanButton">opslaan</button>
+			<button
+				on:click={() => {
+					showAlternativeScreen = !showAlternativeScreen;
+				}}>Terug</button
+			>
+		{/if}
+	</div>
+	<p class="pagination">{index + 1}/3</p>
 </div>
 
 <style>
-.statementScreen {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    align-content: space-around;
-    text-align: center;
-    height: 100vh;
-}
+	.statementScreen {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		align-content: space-around;
+		text-align: center;
+		height: 100vh;
+	}
 
-h1 {
-    color: white;
-}
+	h1 {
+		color: white;
+	}
 
-.greenBox {
-    position: absolute;
-    background-color: #00811f;
-    width: 391px;
-    height: 323px;
-    z-index: -1;
-    top: 0;
-}
+	.emojis {
+		display: flex;
+		justify-content: space-between;
+	}
+	.emoji {
+		width: 30px;
+	}
 
-.emojis {
-    display: flex;
-    justify-content: space-between;
-}
-.emoji {
-    width: 30px;
+	datalist {
+		display: flex;
+		justify-content: space-between;
+	}
 
-}
+	option {
+		color: grey;
+	}
 
-datalist {
-    display: flex;
-    justify-content: space-between;
-}
+	input {
+		width: 50%;
+	}
 
-option {
-    color: grey;
-}
+	.slider {
+		-webkit-appearance: none;
+		width: 100%;
+		height: 15px;
+		border-radius: 5px;
+		background: #d3d3d3;
+		outline: none;
+		opacity: 0.7;
+		-webkit-transition: 0.2s;
+		transition: opacity 0.2s;
+	}
 
-input {
-    width: 50%;
-}
+	.slider:hover {
+		opacity: 1;
+	}
 
-.slider {
-  -webkit-appearance: none;
-  width: 100%;
-  height: 15px;
-  border-radius: 5px;
-  background: #d3d3d3;
-  outline: none;
-  opacity: 0.7;
-  -webkit-transition: .2s;
-  transition: opacity .2s;
-}
+	.slider::-webkit-slider-thumb {
+		-webkit-appearance: none;
+		appearance: none;
+		width: 25px;
+		height: 25px;
+		border-radius: 50%;
+		background: #00811f;
+		cursor: pointer;
+	}
 
-.slider:hover {
-  opacity: 1;
-}
+	.slider::-moz-range-thumb {
+		width: 25px;
+		height: 25px;
+		border-radius: 50%;
+		background: #00811f;
+		cursor: pointer;
+	}
 
-.slider::-webkit-slider-thumb {
-  -webkit-appearance: none;
-  appearance: none;
-  width: 25px;
-  height: 25px;
-  border-radius: 50%;
-  background: #00811f;
-  cursor: pointer;
-}
+	img {
+		width: 100%;
+		margin-bottom: 30px;
+	}
 
-.slider::-moz-range-thumb {
-  width: 25px;
-  height: 25px;
-  border-radius: 50%;
-  background: #00811f;
-  cursor: pointer;
-}
+	span {
+		text-decoration: underline;
+	}
 
-img {
-    width: 100%;
-    margin-bottom: 30px;
-}
+	.green {
+		text-transform: Uppercase;
+		color: #00811f;
+		border-color: #00811f;
+	}
 
-span {
-    text-decoration: underline;
-}
+	.red {
+		text-transform: Uppercase;
+		color: #ff5c50;
+		border-color: #ff5c50;
+	}
 
-.green {
-    text-transform: Uppercase;
-    color: #00811f;
-    border-color: #00811f;
-}
-
-.red {
-    text-transform: Uppercase;
-    color: #FF5C50;
-    border-color: #FF5C50;
-}
-
-.opslaanButton {
-    background-color: #00811f;
-    color: white;
-    border-radius: 4px;
-    font-size: 27px;
-}
-
-.terugButton {
-    background-color: #00811f;
-    color: white;
-    border-radius: 4px;
-    font-size: 27px;
-}
-
-
+	.buttonsOT {
+		background-color: #00811f;
+		color: white;
+		border-radius: 4px;
+		font-size: 27px;
+	}
 </style>
