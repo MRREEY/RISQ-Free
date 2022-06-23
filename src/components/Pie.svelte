b0bf09770b7d96e04486da9607b8dd3df7890148
Qwinsie <theqpowerrr@gmail.com>
2022-06-23 11:59:02 +0200

    Added Pie chart.

---
+++ src/components/Pie.svelte
@@ -0,0 +1,33 @@
+<script>
+    export let size = 250;
+    export let percent = 0;
+    export let bgColor = '#FF5C50';
+    export let fgColor = '#00811f';
+    
+    $: viewBox = `0 0 ${size} ${size}`;
+    
+    $: radius = size / 2;
+    $: halfCircumference = Math.PI * radius;
+    $: pieSize = halfCircumference * (percent / 100);
+    $: dashArray = `0 ${halfCircumference - pieSize} ${pieSize}`;
+  </script>
+  
+  <svg width={size} height={size} {viewBox}>
+    <circle r={radius} cx={radius} cy={radius} fill={bgColor} />
+    <circle
+      r={radius / 2}
+      cx={radius}
+      cy={radius}
+      fill={bgColor}
+      stroke={fgColor}
+      stroke-width={radius}
+      stroke-dasharray={dashArray}
+    />
+  </svg>
+
+  <style>
+
+svg {
+    transform: rotate(270deg);
+}
+  </style>
